c323c0bff569c9678a4cab88d6f8caaedf8212cd
DESKTOP-AT1PNCD\thaonguyen <thaong22.03@gmail.com>
2021-02-28 10:43:33 +0700

    Create an admin login page

---
+++ shopping/app/Http/Controllers/AdminController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\AdminController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AdminController extends Controller
+{
+    public function loginAdmin()
+    {
+    	// dd(bcrypt('thaoxingai')); //bcrypt tạo password mã hóa
+    	return view('login');
+    }
+
+    public function postLoginAdmin(Request $request)
+    {
+    	$remember = $request->has('remember_me') ? true : false;
+    	//auth() pthuc check tài khoản xem hợp lệ hay ko
+    	if(Auth::attempt([
+    		'email' => $request->email,
+    		'password' => $request->password
+    	], $remember)){
+    		return redirect()->to('home');
+    	}
+    }
+}
--- shopping/routes/web.php
+++ shopping/routes/web.php
@@ -13,9 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'App\Http\Controllers\AdminController@loginAdmin');
+
+Route::post('/', 'App\Http\Controllers\AdminController@postLoginAdmin');
 
 Route::get('/home', function () {
     return view('home');
